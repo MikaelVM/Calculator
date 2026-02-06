7ff352ef72ba89244fdad4ec26641316340a5616
Mikael Vind Mikkelsen <mikael.mikkelsen@specialisterne.dk>
2026-02-06 07:01:19 +0100

    First flake8 iteration and testing Typer

---
+++ calculator.py
@@ -0,0 +1,94 @@
+"""A calculator that can perform basic arithmetic operations."""
+import re
+
+
+class Calculator:
+    """A calculator that can perform basic arithmetic operations."""
+
+    def __init__(self):
+        """Initialize the calculator."""
+        self.memory: list[str] = []
+
+    def execute_equation(self, equation: str) -> str:
+        """Execute an equation and return the result.
+
+        Args:
+            equation (str): The equation to execute.
+
+        Returns:
+            str: The result of the equation.
+
+        Raises:
+            ZeroDivisionError: If the equation contains a division by zero.
+            ValueError: If the equation is invalid.
+        """
+        self.memory = re.findall('[+-/*//()]|\\d+', equation)
+
+        while '*' in self.memory:
+            index = self.memory.index('*')
+            result = self.multiplication(float(self.memory[index - 1]), float(self.memory[index + 1]))
+
+            self.memory.pop(index - 1)
+            self.memory.pop(index - 1)
+            self.memory.pop(index - 1)
+
+            self.memory.insert(index - 1, str(result))
+
+        while '/' in self.memory:
+            index = self.memory.index('/')
+
+            if float(self.memory[index + 1]) == 0:
+                raise ZeroDivisionError()
+
+            result = self.division(float(self.memory[index - 1]), float(self.memory[index + 1]))
+
+            self.memory.pop(index - 1)
+            self.memory.pop(index - 1)
+            self.memory.pop(index - 1)
+
+            self.memory.insert(index - 1, str(result))
+
+        while '-' in self.memory:
+            index = self.memory.index('-')
+            result = self.subtraction(float(self.memory[index - 1]), float(self.memory[index + 1]))
+
+            self.memory.pop(index - 1)
+            self.memory.pop(index - 1)
+            self.memory.pop(index - 1)
+
+            self.memory.insert(index - 1, str(result))
+
+        while '+' in self.memory:
+            index = self.memory.index('+')
+            result = self.addition(float(self.memory[index - 1]), float(self.memory[index + 1]))
+
+            self.memory.pop(index - 1)
+            self.memory.pop(index - 1)
+            self.memory.pop(index - 1)
+
+            self.memory.insert(index - 1, str(result))
+
+        if len(self.memory) > 1:
+            raise ValueError("Current input caused an error")
+
+        return self.memory[0]
+
+    @staticmethod
+    def multiplication(number1: float, number2: float) -> float:
+        """Multiply two numbers."""
+        return number1 * number2
+
+    @staticmethod
+    def division(number1: float, number2: float) -> float:
+        """Divide two numbers."""
+        return number1 / number2
+
+    @staticmethod
+    def addition(number1: float, number2: float) -> float:
+        """Add two numbers."""
+        return number1 + number2
+
+    @staticmethod
+    def subtraction(number1: float, number2: float) -> float:
+        """Subtract two numbers."""
+        return number1 - number2
--- main.py
+++ main.py
@@ -1,88 +1,22 @@
-import re
+"""Execution of the command line interface."""
 
-class Calculator:
-    """A calculator"""
+import typer
+from calculator import Calculator
 
-    def __init__(self):
-        self.memory : list[str] = []
+app = typer.Typer()
 
-    def execute_equation(self, equation : str):
-        """Execute an equation in the form of a string"""
-        self.memory = re.findall('[+-/*//()]|\\d+', equation)
+@app.command()
+def hello(name: str):
+    """Prints a simple hello for a given name."""
+    print(f"Hello {name}")
 
-        while '*' in self.memory:
-            index = self.memory.index('*')
-            result = self.multiplication(float(self.memory[index-1]), float(self.memory[index+1]))
-
-            self.memory.pop(index - 1)
-            self.memory.pop(index - 1)
-            self.memory.pop(index - 1)
-
-            self.memory.insert(index - 1, str(result))
-
-        while '/' in self.memory:
-            index = self.memory.index('/')
-
-            if float(self.memory[index+1]) == 0:
-                raise ZeroDivisionError()
-
-            result = self.division(float(self.memory[index-1]), float(self.memory[index+1]))
-
-            self.memory.pop(index - 1)
-            self.memory.pop(index - 1)
-            self.memory.pop(index - 1)
-
-            self.memory.insert(index - 1, str(result))
-
-        while '-' in self.memory:
-            index = self.memory.index('-')
-            result = self.subtraction(float(self.memory[index-1]), float(self.memory[index+1]))
-
-            self.memory.pop(index - 1)
-            self.memory.pop(index - 1)
-            self.memory.pop(index - 1)
-
-            self.memory.insert(index - 1, str(result))
-
-        while '+' in self.memory:
-            index = self.memory.index('+')
-            result = self.addition(float(self.memory[index-1]), float(self.memory[index+1]))
-
-            self.memory.pop(index - 1)
-            self.memory.pop(index - 1)
-            self.memory.pop(index - 1)
-
-            self.memory.insert(index - 1, str(result))
-
-        if len(self.memory) > 1:
-            raise ValueError("Current input caused an error")
-
-        return self.memory[0]
-
-    @staticmethod
-    def multiplication(number1 : float, number2 : float):
-        return number1 * number2
-
-    @staticmethod
-    def division(number1 : float, number2 : float):
-        return number1 / number2
-
-    @staticmethod
-    def addition(number1 : float, number2 : float):
-        return number1 + number2
-
-    @staticmethod
-    def subtraction(number1 : float, number2 : float):
-        return number1 - number2
-
-
+@app.command()
+def goodbye(name: str, formal: bool = False):
+    if formal:
+        print(f"Goodbye Ms. {name}. Have a good day.")
+    else:
+        print(f"Bye {name}!")
 
 
 if __name__ == "__main__":
-    calculator = Calculator()
-
-    print(
-        calculator.execute_equation(
-            input()
-        )
-    )
+    app()
